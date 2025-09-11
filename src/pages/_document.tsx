557d09ac0447bddc45710394508d869b4afe1675
gpalai <gpal2k23@gmail.com>
2025-09-11 01:29:32 +0100

    fixed meta pixel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,8 +10,28 @@ export default function Document() {
         {/* You can also include larger icons for better display on devices like iOS */}
         <link rel="apple-touch-icon" href="/favicon.ico" />
         
-        {/* Other meta tags, like for social media (we’ll address those later for LinkedIn/Facebook) */}
-      </Head>
+        {/* ✅ Meta Pixel (site-wide) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){
+            n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;
+            s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+            (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1436893201055174'); 
+            fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1436893201055174&ev=PageView&noscript=1"
+          />
+        </noscript>      
+        </Head>
       <body className="antialiased">
                 {/* ✅ Google Tag Manager (noscript) */}
         <noscript>
