8b0e6842c368fd76ea7474a9b8960cae97a6151a
gpalai <gpal2k23@gmail.com>
2025-11-02 23:23:14 +0000

    add meta pixel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicons */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
 
@@ -13,7 +14,34 @@ export default function Document() {
           name="facebook-domain-verification"
           content="jbe171dhfq0cgarmzkob3p82jpns9l"
         />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2658350044520902');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2658350044520902&ev=PageView&noscript=1"
+          />
+        </noscript>
       </Head>
+
       <body className="antialiased">
         <Main />
         <script src="//code.tidio.co/owio4o90ktvdhweukqy6pqjube1kjn1u.js" async />
