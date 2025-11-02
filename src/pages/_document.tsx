9385b9771012222e24a1b0277f4961e99ce34835
gpalai <gpal2k23@gmail.com>
2025-11-02 23:31:44 +0000

    added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,23 @@ export default function Document() {
           content="jbe171dhfq0cgarmzkob3p82jpns9l"
         />
 
-        {/* Meta Pixel Code */}
+        {/* Google Analytics (GA4) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-414NQBPTY2"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-414NQBPTY2');
+            `,
+          }}
+        />
+
+        {/* Meta Pixel Code - Aion-clinic.co.uk */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -27,7 +43,7 @@ export default function Document() {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '2658350044520902');
+              fbq('init', '134603103223137');
               fbq('track', 'PageView');
             `,
           }}
@@ -37,14 +53,18 @@ export default function Document() {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2658350044520902&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=134603103223137&ev=PageView&noscript=1"
           />
         </noscript>
       </Head>
 
       <body className="antialiased">
         <Main />
-        <script src="//code.tidio.co/owio4o90ktvdhweukqy6pqjube1kjn1u.js" async />
+        {/* Tidio Chat */}
+        <script
+          src="//code.tidio.co/owio4o90ktvdhweukqy6pqjube1kjn1u.js"
+          async
+        />
         <NextScript />
       </body>
     </Html>
